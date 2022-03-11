714d4f4781c97ac4cb4d1f6f33e3591538ad94ac
bill-the-dev <billthedev@gmail.com>
2022-03-10 23:32:28 -0500

    Remove model validation for email req, reset heroku db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :username, :session_token, presence: true, uniqueness: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
   after_initialize :ensure_session_token
+
+  # removed :email presence validation for heroku bug 
 
   attr_reader :password
 
