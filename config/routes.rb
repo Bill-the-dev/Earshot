1271e7a7f7a5cc337e3ac69c2a0761fc3e6f85e3
bill-the-dev <billthedev@gmail.com>
2022-04-03 16:30:55 -0400

    Add playlist routes, need add song and remove song

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :songs, only: [:index, :show, :create, :destroy]
     resources :albums, only: [:index, :show]
     resources :artists, only: [:index, :show]
+    resources :playlists, only: [:index, :show, :create, :update, :destroy]
+      # add and remove playlist song?
   end
   
   root to: 'static_pages#root'
@@ -18,3 +20,12 @@ end
   # api_users POST   /api/users(.:format)          api/users#create {:default=>{:format=>JSON}}
 # api_session DELETE /api/session(.:format)        api/sessions#destroy {:default=>{:format=>JSON}}
             # POST   /api/session(.:format)        api/sessions#create {:default=>{:format=>JSON}}
+
+
+
+# api_playlists GET    /api/playlists(.:format)     api/playlists#index {:format=>:json}
+#               POST   /api/playlists(.:format)     api/playlists#create {:format=>:json}
+# api_playlist  GET    /api/playlists/:id(.:format) api/playlists#show {:format=>:json}
+#               PATCH  /api/playlists/:id(.:format) api/playlists#update {:format=>:json}
+#               PUT    /api/playlists/:id(.:format) api/playlists#update {:format=>:json}
+#               DELETE /api/playlists/:id(.:format) api/playlists#destroy {:format=>:json}
---
+++ frontend/util/playlist_util.js
@@ -0,0 +1,40 @@
+export const createPlaylist = (playlist) => {
+  return $.ajax({
+    url: `/api/playlists`,
+    method: 'POST',
+    data: {playlist}
+  })
+}
+export const fetchPlaylists = () => {
+  return $.ajax({
+    url: `/api/playlists`,
+    method: 'GET',
+  })
+}
+export const fetchPlaylist = (id) => {
+  return $.ajax({
+    url: `/api/playlists/${id}`,
+    method: 'GET'
+  })
+}
+// export const addPlaylistSong = (playlistId, songId) => {
+//   return $.ajax({
+//     url: `/api/playlists/`,
+//     method: '',
+//     data: {}
+//   })
+// }
+// export const createPlaylist = (playlist) => {
+//   return $.ajax({
+//     url: ``,
+//     method: '',
+//     data: {}
+//   })
+// }
+// export const createPlaylist = (playlist) => {
+//   return $.ajax({
+//     url: ``,
+//     method: '',
+//     data: {}
+//   })
+// }
